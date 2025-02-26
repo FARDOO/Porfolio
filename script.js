c3cdca143b76e6ff55da2506455287d4474ce175
Santiago Fardini <89415821+FARDOO@users.noreply.github.com>
2025-02-26 18:01:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -36,11 +36,11 @@ function efectoHabilidades(){
 
     if(distancia_skills >= 300)
     {
-        document.getElementById("c").classList.add("barra-progreso1");
-        document.getElementById("html").classList.add("barra-progreso2");
-        document.getElementById("java").classList.add("barra-progreso3");
+        document.getElementById("python").classList.add("barra-progreso1");
+        document.getElementById("c++").classList.add("barra-progreso2");
+        document.getElementById("html").classList.add("barra-progreso3");
         document.getElementById("js").classList.add("barra-progreso4");
-        document.getElementById("angular").classList.add("barra-progreso5");
+        document.getElementById("c").classList.add("barra-progreso5");
         document.getElementById("sql").classList.add("barra-progreso6");
     }
 }
